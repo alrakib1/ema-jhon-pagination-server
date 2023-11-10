0487d06b207a58a34a500e3aba56c481011ff967
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-11-11 03:59:03 +0600

    completed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cjx1mmk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -38,6 +38,22 @@ async function run() {
         .toArray();
       res.send(result);
     });
+
+
+    app.post('/productByIds', async(req,res)=>{
+      const ids= req.body;
+      const idsWithObjectId = ids.map(id=> new ObjectId(id))
+      console.log(idsWithObjectId);
+      const query = {
+        _id: {
+          $in : idsWithObjectId
+        }
+      }
+      const result = await productCollection.find(query).toArray();
+      res.send(result)
+    })
+
+
 
     app.get("/productsCount", async (req, res) => {
       const count = await productCollection.estimatedDocumentCount();
